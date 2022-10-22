49c2e488e36c2e1652c70bcc4dd9373cc5b8ba5c
Ascetic <76029322+qascetic@users.noreply.github.com>
2022-10-22 17:18:36 +0800

    202210211944封装注册ui类并修复已知Bug

--- ConnectDatabase.py
+++ ConnectDatabase.py
@@ -2,12 +2,33 @@ import pymysql
 
 
 class ConData:
-    def __init__(self):
-        pass
+    """与数据库握手"""
+    def __init__(self, _host, _user, _password, _port, _db, _charset):
+        """初始化数据库各类参数"""
+        # host
+        self.dbhost = _host
+        # 数据库登录用户名
+        self.dbuser = _user
+        # 数据库登录密码
+        self.dbpassword = _password
+        # 数据库端口
+        self.dbport = _port
+        # 数据库名
+        self.dbname = _db
+        # 数据库字符集
+        self.dbcharset = _charset
 
     def get_con_database(self):
-        conn = pymysql.connect(user='root', password='root', db='student2022')
+        """建立连接并返回连接"""
+        conn = pymysql.connect(host=self.dbhost,
+                               user=self.dbuser,
+                               password=self.dbpassword,
+                               port=self.dbport,
+                               database=self.dbname,
+                               charset=self.dbcharset)
+        # 返回建立的连接
         return conn
 
     def clos_con(self, conn):
+        """关闭数据库连接"""
         conn.close()
--- Login.py
+++ Login.py
@@ -1,150 +1,182 @@
 ﻿# -*- coding:utf-8 -*-
-import os
-import pymysql
-import time
 import tkinter
 import tkinter.messagebox
+
+import pymysql
+
 import Student
-import Teacher
 import SystemManager
+import Teacher
+import register_ui
 
 
 class Login:
-    def __init__(self, conn):
+    """处理登录函数"""
+
+    def __init__(self, _conn):
+        # 用户名
         self.account = ''
+        # 密码
         self.password = ''
+        # 用户等级
         self.level = 2
-        self.conn = conn
+        # 数据库连接
+        self.conn = _conn
+        # 密码可错误次数
         self.err_time = 3
 
     def MainFunc(self, _gui_root, _account, _password):
-        err = ''
+        """登录"""
+        # 用户名
         self.account = _account
+        # 密码
         self.password = _password
+        # 试错机会是否耗尽
         if self.err_time <= 0:
+            # 密码输入错误太多
             tkinter.messagebox.showerror(title='教务管理系统', message='警告！输入错误次数太多！系统自动退出')
+            # 关闭登录界面
             _gui_root.destroy()
-        elif self.account == '':
-            tkinter.messagebox.showerror(title='教务管理系统', message='警告！用户名不能为空！')
-        elif self.CheckAccount():
-            tkinter.messagebox.showinfo(title='教务管理系统', message='登录成功')
-            tkinter.messagebox.showinfo(title='教务管理系统', message='请稍后...')
-            _gui_root.destroy()
-            account = self.GetLoginAccount()
-            if account[2] == 0:
-                usr = SystemManager.SystemManager(self.conn, account[0], account[1])
-                usr.MainFunc()
-            elif account[2] == 1:
-                usr = Teacher.Teacher(self.conn, account[0], account[1])
-                usr.MainFunc()
-            elif account[2] == 2:
-                usr = Student.Student(self.conn, account[0], account[1])
-                usr.MainFunc()
         else:
-            self.err_time = self.err_time - 1
-            tkinter.messagebox.showerror(title='教务管理系统',
-                                         message='用户名或密码错误!\n您还能输入' + str(self.err_time) + '次')
-    def CheckAccount(self):
+            # 用户名不能为空
+            if self.account == '':
+                tkinter.messagebox.showerror(title='教务管理系统', message='警告！用户名不能为空！')
+            else:
+                # 判断密码是否正确并获取判断结果
+                checkreturn = self.CheckAccount(_gui_root)
+                if "ok" == checkreturn:
+                    # 密码正确
+                    tkinter.messagebox.showinfo(title='教务管理系统', message='登录成功')
+                    tkinter.messagebox.showinfo(title='教务管理系统', message='请稍后...')
+                    # 关闭登录界面
+                    _gui_root.destroy()
+                    # 获取用户身份等级
+                    account = self.GetLoginAccount()
+                    # 管理员
+                    if account[2] == 0:
+                        usr = SystemManager.SystemManager(self.conn, account[0], account[1])
+                        usr.MainFunc()
+                    # 教职工
+                    elif account[2] == 1:
+                        usr = Teacher.Teacher(self.conn, account[0], account[1])
+                        usr.MainFunc()
+                    # 学生
+                    elif account[2] == 2:
+                        usr = Student.Student(self.conn, account[0], account[1])
+                        usr.MainFunc()
+                elif "err" == checkreturn:
+                    # 密码错误
+                    # 试错机会减一
+                    self.err_time = self.err_time - 1
+                    # 提示用户剩余可用机会
+                    tkinter.messagebox.showerror(title='教务管理系统',
+                                                 message='用户名或密码错误!\n您还能输入' + str(self.err_time) + '次')
+                else:
+                    # 用户注册了新账户
+                    pass
+
+    def CheckAccount(self, _ui_root):
+        """判断密码是否正确"""
+        # 数据库游标
         cur = self.conn.cursor()
+        # 用于数据库查询的语句
         sqlcmd = "select Account,Password,AccountLevel from LoginAccount where Account = '%s'" % self.account
+        # 未查询到任何匹项
         if cur.execute(sqlcmd) == 0:
             login_s = tkinter.messagebox.askokcancel(title='教务管理系统',
                                                      message='您输入的用户名不存在，您需要要创建新用户吗')
-        else:
-            temp = cur.fetchone()
-            cur.close()
-            if temp[1] == self.password:
-                self.level = temp[2]
-                return True
+            # 用户试图注册新账户
+            if login_s:
+                self.login_res(_ui_root)
+                return "res"
+            # 用户不打算注册新账户
             else:
-                return False
+                return "err"
+        # 查询到用户信息
+        else:
+            # 获取用户信息
+            temp = cur.fetchone()
+            # 关闭游标
+            cur.close()
+            # 匹配用户输入的密码和数据库中的密码
+            # 密码匹配成功
+            if temp[1] == self.password:
+                # 获取用户等级
+                self.level = temp[2]
+                # 允许登录
+                return "ok"
+            else:
+                # 不允许登录
+                return "err"
 
     def GetLoginAccount(self):
+        """获取用户等级"""
+        # 返回用户等级
         return [self.account, self.password, self.level]
 
     def login_res(self, _root):
-        def login_run():
-            cur = self.conn.cursor()
-            # 用get提取
-            new_user_name = new_var_name.get()
-            new_user_real_name = new_var_user_name.get()
-            new_user_password = new_var_password.get()
-            repeat_user_password = repeat_var_password.get()
+        """注册"""
 
+        def login_run(_res_ui, _user, _real, _password, _repassword):
+            """核验注册信息"""
+            cur = self.conn.cursor()
+            # 用get提取用户注册信息
+            # 获取用户名
+            new_user_name = _user
+            # 获取真实姓名
+            new_user_real_name = _real
+            # 获取注册密码
+            new_user_password = _password
+            # 获取重复密码
+            repeat_user_password = _repassword
+            # 校验两次密码
             if new_user_password != repeat_user_password:
                 tkinter.messagebox.showerror(title='教务管理系统', message='警告！您输入的两次密码不一致！')
-                """"""
+            # 当前真实用户名是否为空
+            elif new_user_real_name == '':
+                tkinter.messagebox.showerror(title='教务管理系统', message='警告！姓名不能为空！')
+            # 当前用户名是否为空
+            elif new_user_name == '':
+                tkinter.messagebox.showerror(title='教务管理系统', message='警告！用户名不能为空！')
+            # 当前密码是否为空
+            elif new_user_password == '':
+                tkinter.messagebox.showerror(title='教务管理系统', message='警告！密码不能为空！')
+            # 当前用户名是否存
             elif 0 != cur.execute("select Account,Password,AccountLevel "
                                   "from LoginAccount where Account = '%s'" % new_user_name):
                 tkinter.messagebox.showerror(title='教务管理系统', message='警告！您输入的用户名已存在！')
-            elif new_user_real_name == '':
-                tkinter.messagebox.showerror(title='教务管理系统', message='警告！姓名不能为空！')
-            elif new_user_name == '':
-                tkinter.messagebox.showerror(title='教务管理系统', message='警告！用户名不能为空！')
-            elif new_user_password == '':
-                tkinter.messagebox.showerror(title='教务管理系统', message='警告！密码不能为空！')
             else:
-                # login_user_info[new_user_name] = new_user_password
                 try:
                     # insert into loginaccount(Account, Password, AccountLevel, sname) values('2002', '123', '2', '456')
-                    sqlWord = "insert into loginaccount(Account, Password, AccountLevel, sname) values ('%s', '%s', '2', '%s')" % (
-                        new_user_name, new_user_password, new_user_real_name)
-                    cur.execute(sqlWord)
+                    # mysql插入语句
+                    sql_word = "insert into loginaccount(Account, Password, AccountLevel, sname) values ('%s', '%s', " \
+                               "'2', '%s')" % (new_user_name, new_user_password, new_user_real_name)
+                    # 执行mysql语句插入数据
+                    cur.execute(sql_word)
+                    # 提交数据
                     self.conn.commit()
                     tkinter.messagebox.showinfo(title='教务管理系统', message='注册成功，欢迎您')
                 except Exception as e:
-                    print(e)
+                    # 提示异常
                     tkinter.messagebox.showinfo(title='教务管理系统', message='注册失败，错误' + e.__str__())
                 finally:
-                    root_login.destroy()
+                    # 关闭注册界面
+                    _res_ui.destroy()
+            # 关闭游标
             cur.close()
 
-        root_login = tkinter.Toplevel(_root)
-        root_login.title('教务管理系统')
-        root_login.geometry('350x350+550+200')
-        a = tkinter.Label(root_login, text='欢迎进入注册界面', font=('Arial', 10))
-        a.pack()
+        # 建立注册UI需要提供根界面
+        res_ui = register_ui.RegisterUi(_root)
+        # 开启登录UI
+        # 需要提供注册信息函数
+        res_ui.run_ui(login_run)
 
-        # 注册窗口界面
-        # 注册用户名
-        new_var_name = tkinter.StringVar()  # 将输入框里面的东西拿出来
-        show_new_name = tkinter.Label(root_login, text='新用户名', font=('Arial', 9))
-        show_new_name.place(x=70, y=50)
-        input_new_name = tkinter.Entry(root_login, textvariable=new_var_name, show=None)
-        input_new_name.place(x=125, y=50)
-
-        # 注册密码
-        new_var_password = tkinter.StringVar()  # 将输入框里面的东西拿出来
-        show_new_password = tkinter.Label(root_login, text='新的密码', font=('Arial', 9))
-        show_new_password.place(x=70, y=100)
-        input_new_password = tkinter.Entry(root_login, textvariable=new_var_password, show='*')
-        input_new_password.place(x=125, y=100)
-
-        # 重复注册密码
-        repeat_var_password = tkinter.StringVar()  # 将输入框里面的东西拿出来
-        show_repeat_password = tkinter.Label(root_login, text='重复密码', font=('Arial', 9))
-        show_repeat_password.place(x=70, y=150)
-        input_repeat_password = tkinter.Entry(root_login, textvariable=repeat_var_password, show='*')
-        input_repeat_password.place(x=125, y=150)
-
-        new_var_user_name = tkinter.StringVar()  # 将输入框里面的东西拿出来
-        show_new_user_name = tkinter.Label(root_login, text='姓名', font=('Arial', 9))
-        show_new_user_name.place(x=70, y=200)
-        input_new_user_name = tkinter.Entry(root_login, textvariable=new_var_user_name, show=None)
-        input_new_user_name.place(x=125, y=200)
-
-        # 注册界面按钮
-        login_button1 = tkinter.Button(root_login, text='注册', width=7, command=login_run)
-        login_button1.place(x=115, y=230)
-        login_button2 = tkinter.Button(root_login, text='取消', width=7, command='quit')
-        login_button2.place(x=205, y=230)
-
+    def forgot_password(self):
+        """忘记密码"""
+        tkinter.messagebox.showinfo(title='教务管理系统', message='请联系统管理员重置密码！')
 
 
 if __name__ == '__main__':
     conn = pymysql.connect(user='root', passwd='root', db='student2022');
     a = Login(conn)
-    # a.MainFunc()
-    a.login_res()
-    a.Quit()
     conn.close()
